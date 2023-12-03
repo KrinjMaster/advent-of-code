0f12794651a43db5cbdde1f30d60f4940adf507a
KrinjMaster <5opkasuka@gmail.com>
2023-12-03 21:10:32 +0300

    day 2 task 1 solution (js)

--- 2023/01/a.js
+++ 2023/01/a.js
@@ -1,8 +1,9 @@
-import * as fs from "fs";
+const fs = require("fs");
 
 let totalSum = 0;
 
 const data = fs.readFileSync("./text.txt", "utf-8").split("\n");
+
 for (let i = 0; i < data.length - 1; i++) {
   const numString = data[i].replaceAll(/[a-z]/g, "");
 
--- 2023/01/b.js
+++ 2023/01/b.js
@@ -1,4 +1,4 @@
-import * as fs from "fs";
+const fs = require("fs");
 
 let totalSum = 0;
 const spelledLetters = [
