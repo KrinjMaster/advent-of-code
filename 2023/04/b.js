0a4e436c1c55e10778b9f9bd65b8a60cdc6a1632
KrinjMaster <5opkasuka@gmail.com>
2023-12-05 20:04:52 +0300

    refactored day 4 solution

--- 2023/04/b.js
+++ 2023/04/b.js
@@ -13,12 +13,24 @@ const data = fs
         .map((str) => Number(str)),
     ),
   )
-  .map((arr, index) => {
+  .map((arr) => {
     return arr[1]
       .map((val) => (arr[0].includes(val) ? 1 : 0))
       .reduce((val, prev) => val + prev);
   })
-  .map((val) => (val > 0 ? Math.pow(2, val - 1) : 0))
+  .map((el) => [el, 1])
+  .map((val, index, arr) => {
+    const newArr = arr;
+
+    for (let j = 0; j < val[1]; j++) {
+      for (let i = 0; i < val[0]; i++) {
+        newArr[i + index + 1][1] += 1;
+      }
+    }
+
+    return newArr[index];
+  })
+  .map((el) => el[1])
   .reduce((val, prev) => val + prev);
 
 console.log(data);
